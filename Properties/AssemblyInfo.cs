4707affa40e755dd2d31e6eaa34ad04091b806b8
Dezerdvs <makentosh783@gmail.com>
2023-11-27 10:39:06 +0200

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestForLab03")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestForLab03")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db8d00fc-f269-46b9-951d-0284483205a1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab03;
+
+namespace UnitTestForLab03
+{
+    [TestClass]
+    public class DogTests
+    {
+        [TestMethod]
+        public void Labrador_GetAmountFoodPerDay_ReturnsCorrectValue()
+        {
+            Labrador labrador = new Labrador("Labrador", 0.5, 20);
+            double amountFoodPerDay = labrador.GetAmountFoodPerDay();
+            Assert.AreEqual(8, amountFoodPerDay); 
+        }
+
+        [TestMethod]
+        public void Labrador_ToString_FormatsCorrectly()
+        {
+            Labrador labrador = new Labrador("Labrador", 0.5, 20);
+            string result = labrador.ToString();
+            Assert.AreEqual("Labrador. Висота: 0,5. Вага: 20. Їжі в день: 8", result);
+        }
+
+        [TestMethod]
+        public void Yorkshire_GetAmountFoodPerDay_ReturnsCorrectValue()
+        {
+
+            Yorkshire yorkshire = new Yorkshire("Yorkshire", 10, 2);
+            double amountFoodPerDay = yorkshire.GetAmountFoodPerDay();
+            Assert.AreEqual(5, amountFoodPerDay); 
+        }
+
+        [TestMethod]
+        public void Yorkshire_ToString_FormatsCorrectly()
+        {
+            Yorkshire yorkshire = new Yorkshire("Yorkshire", 10, 2);           
+            string result = yorkshire.ToString();
+            Assert.AreEqual("Yorkshire. Довжина: 10. Вага: 2. Їжі в день: 5", result);
+        }
+    }
+}
